4092ba58b4ec7a6839ecbf8cb60ca07e873efb94
Jatin Joshi <joshijatin55@gmail.com>
2020-03-25 12:39:39 +0530

    first project

---
+++ Clockworks/script.js
@@ -0,0 +1,108 @@
+let timerObj = {
+    minutes: 0,
+    seconds: 0,
+    timerId: 0
+}
+
+function soundAlarm() {
+  let amount = 3;
+  let audio = new Audio("Timer_Sound_Effect.mp3");
+
+  function playSound() {
+      audio.pause();
+      audio.currentTime = 0;
+      audio.play();
+  }
+
+  for(let i = 0; i < amount; i++) {
+      setTimeout(playSound,1200*i);
+  }
+}
+
+function updateValue(key,value) {
+    if(value < 0) {
+      value = 0;
+      console.log("Positive Numbers Only");
+    }
+
+    if(key == "seconds") {
+      if(value < 10) {
+          value = "0" + value;
+      }
+    }
+
+    if(value > 59) {
+      value = 59;
+    }
+
+    $("#" + key).html(value || 0);
+    timerObj[key] = value;
+}
+
+(function detectChanges(key) {
+    let input = "#" + key + "-input";
+
+    $(input).change(function(){
+        updateValue(key,$(input).val());
+    });
+
+    $(input).keyup(function(){
+        updateValue(key,$(input).val());
+    });
+
+    return arguments.callee;
+
+})("minutes")("seconds");
+
+function startTimer() {
+    buttonManager(["start",false],["pause",true],["stop",true]);
+    freezeInputs();
+
+    timerObj.timerId = setInterval(function(){
+        timerObj.seconds--;
+        if(timerObj.seconds < 0) {
+            if(timerObj.minutes == 0) {
+                soundAlarm();
+                return stopTimer();
+            }
+            timerObj.seconds = 59;
+            timerObj.minutes--;
+        }
+        updateValue("minutes", timerObj.minutes);
+        updateValue("seconds", timerObj.seconds);
+    },1000);
+}
+
+function stopTimer() {
+    clearInterval(timerObj.timerId);
+    buttonManager(["start",true],["pause",false],["stop",false]);
+    unfreezeInputs();
+    updateValue("minutes",$("#minutes-input").val());
+    updateValue("seconds",$("#seconds-input").val());
+}
+
+function pauseTimer() {
+    buttonManager(["start",true],["pause",false],["stop",true]);
+    clearInterval(timerObj.timerId);
+
+}
+
+function buttonManager(...buttonArray) {                               /*Rest Operator*/
+    for(let i = 0; i < buttonArray.length ; i++) {
+        let button = "#" + buttonArray[i][0] + "-button";
+        if(buttonArray[i][1]) {
+            $(button).removeAttr("disabled");
+        } else {
+            $(button).attr("disabled","disabled");
+        }
+    }
+}
+
+function freezeInputs() {
+    $("#minutes-input").attr("disabled","disabled");
+    $("#seconds-input").attr("disabled","disabled");
+}
+function unfreezeInputs() {
+    $("#minutes-input").removeAttr("disabled","disabled");
+    $("#seconds-input").removeAttr("disabled","disabled");
+}
